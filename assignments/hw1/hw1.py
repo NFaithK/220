152bee5c42751ed4e6e9a33e57e1c5b217e6cf84
NFaithK <97078366+NFaithK@users.noreply.github.com>
2022-01-19 10:45:16 -0500

    Assigment 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,49 @@
 """
-Name: <your name goes here – first and last>
+Name: <Faith Kelley>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: < The problem we are solving would  be  common ways,  by using formulas for this coding assignment you will
+be able to  get the correct amounts >
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
-"""
+" I certify this is my own work"""
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("enter the length:"))
+    width = eval(input("enter the width:"))
+    height = eval(input("enter the height:"))
+    volume = length * width * height
+    print("Volume=", volume)
 
 
 def shooting_percentage():
-    pass
+    shots = eval(input("enter how many  total shots: "))
+    made = eval(input("enter how many shots the player made: "))
+    shootingpercentage = made / shots * 100
+    print("shooting percentage= ", shootingpercentage,"%")
 
 
 def coffee():
-    pass
+    pounds = eval(input("enter how many pounds of  coffee would  you like: "))
+    totalcost = 10.50 * pounds + 0.86 * pounds + 1.50
+    print("your total is: ", totalcost)
 
 
 def kilometers_to_miles():
-    pass
+    kilo = eval(input("how many kilometers did you travel?: "))
+    miles = kilo * 0.621371
+    print("thats", miles, "miles!")
 
 
 if __name__ == '__main__':
