bb3af194194cb31b0f3d55638a84fe3376218e30
NFaithK <97078366+NFaithK@users.noreply.github.com>
2022-01-19 13:33:26 -0500

    Assigment 1

---
+++ notes/Chapter 1 notes.py
@@ -0,0 +1,13 @@
+def future():
+    age = eval(input( "enter your age: "))
+    futureage = age + 20
+    print('in 20 years you will be', futureage)
+
+
+def convert():
+    print("lets convert!")
+    celsius = eval(input("what is the temp in celsius?"))
+    fahrenheit = celsius * 9 / 5 + 32
+    print("that temperature is", fahrenheit, "degrees fahrenheit.")
+
+
