997a4f5930d07bf75be0fbb821c8beb49fc5a957
NFaithK <97078366+NFaithK@users.noreply.github.com>
2022-01-27 00:11:30 -0500

    Homework 2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -2,7 +2,8 @@
 Name: <your name goes here – first and last>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Brief, one or two sentence description of the problem
+ that this program solves, in your own words.>
 
 Certification of Authenticity:
 <include one of the following>
@@ -13,23 +14,51 @@ import math
 
 
 def sum_of_threes():
-    pass
+    multiple_sum = 0
+    upper_bound = eval(input("what is your upper bound: "))
+    for i in range(upper_bound):
+        multiple_sum = (upper_bound + multiple_sum)
+    print(multiple_sum)
+
+
+print()
 
 
 def multiplication_table():
-    pass
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end='\t')
+
+    print('')
 
 
 def triangle_area():
-    pass
+    side_a = eval(input("Enter side a length: "))
+    side_b = eval(input("enter side b length: "))
+    side_c = eval(input("enter side c length: "))
+    side_s = side_a + side_b + side_c
+    side_s = side_s / 2
+    area_s1 = side_s - side_a
+    area_s2 = side_s - side_b
+    area_s3 = side_s - side_c
+    area_t = side_s * area_s1 * area_s2 * area_s3
+    area_t = math.sqrt(area_t)
+    print(area_t)
 
 
 def sum_squares():
-    pass
+    lower_range = eval(input("enter your lower range:"))
+    upper_range = eval(input("enter your upper range:"))
+    range_r = list(range(lower_range, upper_range + 1))
+
+    print(range_e)
 
 
 def power():
-    pass
+    base = eval(input("Enter base:"))
+    exponent = eval(input("enter exponent: "))
+    answer = base ** exponent
+    print(base, "^", exponent, "=", answer)
 
 
 if __name__ == '__main__':
