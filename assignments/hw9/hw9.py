5733b10d5390c66d426fb88851836dea2bee0852
NfaithK <kelleyf@g.cofc.edu>
2022-03-31 22:18:32 -0400

    Lab!

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -1,25 +1,48 @@
+from random import randint
+
+
 def get_words(file_name):
-    pass
+    file_name = open(file_name, 'r')
+    file_name = file_name.readlines()
+    return file_name
 
 
 def get_random_word(words):
-    pass
+    random_word = randint(0, len(words) - 1)
+    secret_word = words[random_word].strip('\n')
+    return secret_word
 
 
 def letter_in_secret_word(letter, secret_word):
-    pass
+    letter = input('Guess a letter')
+    if letter in secret_word:
+        return True
+    return False
 
 
 def already_guessed(letter, guesses):
-    pass
+    if letter in guesses:
+        return True
+    return False
 
 
 def make_hidden_secret(secret_word, guesses):
-    pass
+    word = ""
+    for letter in secret_word:
+        if letter in guesses:
+            word = word + letter + " "
+        else:
+            word = word + "_ "
+    guessed_word = word.strip()
+    return guessed_word
 
 
 def won(guessed):
-    pass
+    guessed = guessed.split(' ')
+    not_guessed = guessed.count("_")
+    if not_guessed == 0:
+        return True
+    return False
 
 
 def play_graphics(secret_word):
@@ -27,7 +50,11 @@ def play_graphics(secret_word):
 
 
 def play_command_line(secret_word):
-    pass
+    print("Already guessed")
+    get_random_word(get_words('words.txt'))
+    letter = input("Guess a letter:")
+    letter_in_secret_word(letter, secret_word)
+    already_guessed(letter, guesses)
 
 
 if __name__ == '__main__':
